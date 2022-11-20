904ed858e3b5520bbcc96c04a97d72822ec24fc3
是夜在熬我 <VClemonTea@outlook.com>
2022-11-20 22:40:22 +0800

    init

---
+++ PowerConsole-Spring/src/main/java/com/vclemoncha/powerconsolespring/PowerConsoleSpringApplication.java
@@ -0,0 +1,13 @@
+package com.vclemoncha.powerconsolespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerConsoleSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PowerConsoleSpringApplication.class, args);
+    }
+
+}
---
+++ PowerConsole-Spring/src/test/java/com/vclemoncha/powerconsolespring/PowerConsoleSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vclemoncha.powerconsolespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowerConsoleSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
